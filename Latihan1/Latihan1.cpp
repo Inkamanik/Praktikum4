5ebccb9d7d43f405b4d22821d4857b486c64740e
Inkamanik <Inkamanik21@gmail.com>
2018-11-06 14:33:53 +0700

    Latihan1

---
+++ Latihan1/Latihan1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int max, a;
+    max = 0;
+
+    do {
+        cout << "Masukan bilangan : ";
+        cin >> a;
+        if (max < a)
+            max = a;
+    } while ( a != 0);
+    cout << "Bilangan terbesar adalah: " << max << endl;
+    return 0;
+}
+
+
+
